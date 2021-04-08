b4d7d998756342888a6921fd2adbfd0b16d2867c
Dan Mercer <danielmercer1@gmail.com>
2021-04-07 21:11:56 -0600

    Add new v2 injector

---
+++ injector.spec.ts
@@ -0,0 +1,40 @@
+import { injectable, Injector, InjectorKey } from './injector';
+
+const A = injectable('A', [], () => {
+  return {
+    foo: 'a',
+  };
+});
+
+const B = injectable('B', [A] as const, (a) => {
+  function getA(): unknown {
+    return a;
+  }
+
+  return {
+    getA,
+    bar: 'b' + a.foo,
+  };
+});
+
+const C = injectable('C', [A, B, InjectorKey] as const, (a, b, injector) => {
+  return {
+    bagel: 'c' + a.foo + b.bar,
+    injector,
+  };
+});
+
+describe('injector v2', () => {
+  it('should work', () => {
+    const injector = new Injector();
+    const c = injector.get(C);
+    const b = injector.get(B)
+    const a = injector.get(A);
+
+    expect(b.bar).toEqual('ba');
+    expect(c.bagel).toEqual('caba');
+
+    expect(b.getA()).toBe(a);
+    expect(c.injector).toBe(injector);
+  })
+})
---
+++ injector.ts
@@ -0,0 +1,67 @@
+class InjectableKey<T> {
+  private IfYoureSeeingThisInAnErrorMessageItMeansYoureTryingToUseSomethingAsAnInjectableKeyWhenItsNotOne!: T;
+  constructor(
+    public injectableName: string,
+  ) { }
+}
+export type { InjectableKey };
+
+type InjectableValue<IK> = IK extends InjectableKey<infer T> ? T : never;
+
+type InjectableValues<Deps> = {
+  [K in keyof Deps]: InjectableValue<Deps[K]>;
+}
+
+interface InjectableData<T> {
+  deps: readonly InjectableKey<unknown>[];
+  factory: (...args: any) => T;
+}
+
+const metadata = new WeakMap<InjectableKey<unknown>, InjectableData<unknown>>();
+
+export function injectable<T, Deps extends readonly InjectableKey<unknown>[]>(name: string, deps: Deps, factory: (...args: InjectableValues<Deps>) => T): InjectableKey<T> {
+  const key = new InjectableKey<T>(name);
+  metadata.set(key, {
+    deps,
+    factory,
+  });
+  return key;
+}
+
+function getInjectableData<T>(key: InjectableKey<T>) {
+  return metadata.get(key) as InjectableData<T>;
+}
+
+export const InjectorKey: InjectableKey<Injector> = new InjectableKey('Injector');
+
+export class Injector {
+  private instances: WeakMap<InjectableKey<unknown>, any> = new WeakMap();
+  /** Only for usage from debugger */
+  private byName: Record<string, any> | undefined = this.debug ? {} : undefined;
+
+  constructor(private debug: boolean = false) { }
+
+  public get<T>(key: InjectableKey<T>): T {
+    if (this.byName && typeof key === 'string') {
+      return this.byName[key];
+    }
+    if (this.instances.has(key)) {
+      return this.instances.get(key);
+    }
+    if (key === InjectorKey as unknown) {
+      return this as unknown as T;
+    }
+    const instance = this.create(key);
+    this.instances.set(key, instance);
+    if (this.byName) {
+      this.byName[key.injectableName] = instance;
+    }
+    return instance;
+  }
+
+  private create<T>(key: InjectableKey<T>): T {
+    const { factory, deps } = getInjectableData(key);
+    const depValues = deps.map(dep => this.get(dep));
+    return factory(...depValues);
+  }
+}
